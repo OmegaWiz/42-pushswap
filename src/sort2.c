b9466fd5aa62f6592b8cd242d8c09f1968933ddb
Kraiwish Kaiaywong <kkaiyawo@z2t17c3.42bangkok.com>
2023-05-04 16:44:02 +0700

    fix swap_rev_2

--- src/sort2.c
+++ src/sort2.c
@@ -6,7 +6,7 @@
 /*   By: kkaiyawo <kkaiyawo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 14:07:49 by kkaiyawo          #+#    #+#             */
-/*   Updated: 2023/05/04 16:26:13 by kkaiyawo         ###   ########.fr       */
+/*   Updated: 2023/05/04 16:43:04 by kkaiyawo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,16 @@ void	ft_push_swap_2(t_stack *sa)
 	}
 }
 
-void	ft_push_swap_2_rev(t_stack *sa)
+void	ft_push_swap_2_rev(t_stack *sb)
 {
 	long	a;
 	long	b;
 
-	a = sa->top->next->data;
-	b = sa->top->next->next->data;
+	a = sb->top->next->data;
+	b = sb->top->next->next->data;
 	if (a < b)
 	{
-		ft_swap(sa);
-		ft_putstr_fd("sa\n", 1);
+		ft_swap(sb);
+		ft_putstr_fd("sb\n", 1);
 	}
 }
