f376b33a706822b05508842dab59647ef1853bbe
Kraiwish Kaiaywong <kkaiyawo@z2t17c3.42bangkok.com>
2023-05-06 16:39:54 +0700

    sort8

--- src/sort8.c
+++ src/sort8.c
@@ -6,13 +6,13 @@
 /*   By: kkaiyawo <kkaiyawo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 16:05:17 by kkaiyawo          #+#    #+#             */
-/*   Updated: 2023/05/04 16:40:24 by kkaiyawo         ###   ########.fr       */
+/*   Updated: 2023/05/06 16:39:43 by kkaiyawo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_push_swap_8_cont(t_stack *sa, t_stack *sb)
+void	ft_push_swap_8_cont(t_node *sa, t_node *sb)
 {
 	int	i;
 
@@ -21,36 +21,34 @@ void	ft_push_swap_8_cont(t_stack *sa, t_stack *sb)
 	i = 0;
 	while (++i <= 4)
 	{
-		ft_push(sa, sb->top->next->data);
-		ft_pop(sb);
+		ft_push(sb, sa);
 		ft_putstr_fd("pa\n", 1);
-		ft_print_stack(sa, sb);
+		ft_print_stack2(sa, sb);
 	}
 }
 
-void	ft_push_swap_8(t_stack *sa, t_stack *sb)
+void	ft_push_swap_8(t_node *sa, t_node *sb)
 {
 	int		i;
 	long	mid;
 
 	if (is_sorted(sa))
 		return ;
-	mid = find_mid(sa, 8);
+	mid = ft_stack_size(sa) / 2;
 	i = -1;
 	while (++i < 8)
 	{
-		if (sa->top->next->data < mid)
+		if (sa->index < mid)
 		{
-			ft_push(sb, sa->top->next->data);
-			ft_pop(sa);
+			ft_push(sa, sb);
 			ft_putstr_fd("pb\n", 1);
-			ft_print_stack(sa, sb);
+			ft_print_stack2(sa, sb);
 		}
 		else
 		{
 			ft_rotate(sa);
 			ft_putstr_fd("ra\n", 1);
-			ft_print_stack(sa, sb);
+			ft_print_stack2(sa, sb);
 		}
 	}
 	ft_push_swap_8_cont(sa, sb);
